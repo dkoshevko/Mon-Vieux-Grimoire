15d44f484c8178801f189eb7c37f8aa54ad156bf
dkoshevko <121438170+dkoshevko@users.noreply.github.com>
2023-06-25 18:12:37 +0200

    Optimisation d'images

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,31 +1,27 @@
 // Importer le module  "multer"
 const multer = require('multer');
+const SharpMulter  =  require('sharp-multer');
 
 // Définition des types MIME acceptés pour les fichiers image
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
-    'image/png': 'png'
+    'image/png': 'png',
+    'image/webp': 'webp'
 };
 
-const storage = multer.diskStorage({
+const storage = SharpMulter({
 
     // Définir le dossier de destination des fichiers téléchargés
     destination: (req, file, callback) => {
         callback(null, 'images')
     },
     
-    // Générer un nom de fichier unique
-    filename: (req, file, callback) => {
-
-        // Remplacer les espaces par des underscores
-        const name = file.originalname.split(' ').join('_');
-
-        // Obtenir l'extension du fichier en fonction de son type MIME
-        const extension = MIME_TYPES[file.mimetype];
-
-        // Concaténer le nom, un horodatage et l'extension pour former le nom de fichier final
-        callback(null, name + Date.now() + '.' + extension)
+    imageOptions: {
+        fileFormat: 'webp',
+        quality: 60,
+        resize: { width: 463, height: 595 },
+        useTimestamp: true,
     }
 });
 
